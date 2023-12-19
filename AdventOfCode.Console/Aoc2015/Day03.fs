175cf9fb0465bb57eb911956ca3379fe17e43f13
Joshua Rakita <joraki@microsoft.com>
2023-12-18 23:40:04 -0800

    Working on 2015-03.1

---
+++ AdventOfCode.Console/Aoc2015/Day03.fs
@@ -0,0 +1,18 @@
+﻿module Aoc2015.Day03
+
+type Point = (int * int)
+
+let pointApplyDirection (point: Point, c: char): Point = 
+    let x', y' = 
+        match c with
+        | '^' -> (0, 1)
+        | 'v' -> (0, -1)
+        | '<' -> (-1, 0)
+        | '>' -> (1, 0)
+        | _ -> (0, 0)
+    let x, y = point
+    (x + x', y + y')
+
+let visited (initialSet: Set<Point>) (directions: string) : Set<Point> =
+    for c in directions do
+        
