4a06e366dd3287f0c3053e384a7cb8f1c48959aa
Joshua Rakita <rakitaj@users.noreply.github.com>
2025-12-01 21:10:05 -0800

    Fixing F# test discovery and running.

--- AdventOfCode.Tests/Aoc2015Tests.fs
+++ AdventOfCode.Tests/Aoc2015Tests.fs
@@ -3,52 +3,49 @@ namespace Tests.Year2015
 open Aoc2015.Day01
 open Aoc2015.Day02
 open Aoc2015.Day03
-open FsUnit.MsTest
-open Microsoft.VisualStudio.TestTools.UnitTesting
+open Xunit
+open FsUnit.Xunit
 
-[<TestClass>]
-type Day01Tests () =
+module Day01Tests =
 
-    [<TestMethod>]
-    [<DataRow("(())", 0)>]
-    [<DataRow("(()(()(", 3)>]
-    member this.``Test finding the floor num``(line: string, expected: int) =
+    [<Theory>]
+    [<InlineData("(())", 0)>]
+    [<InlineData("(()(()(", 3)>]
+    let ``Test finding the floor num``(line: string, expected: int) =
         let floorNum = calcFloor line
         floorNum |> should equal expected
 
-    [<TestMethod>]
-    [<DataRow(")", 1)>]
-    [<DataRow("()())", 5)>]
-    [<DataRow("())()", 3)>]
-    member this.``Test finding the first bastment index`` (line: string, expected: int) =
+    [<Theory>]
+    [<InlineData(")", 1)>]
+    [<InlineData("()())", 5)>]
+    [<InlineData("())()", 3)>]
+    let ``Test finding the first bastment index`` (line: string, expected: int) =
         let minbasementIdex = floorNumRunningTotal line |> firstNegativeFloor
         minbasementIdex |> should equal expected
 
-[<TestClass>]
-type Day02Tests () = 
+module Day02Tests = 
 
-    [<TestMethod>]
-    [<DataRow("2x3x4", 58)>]
-    [<DataRow("1x1x10", 43)>]
-    member this.``Test calculating the surface area of the wrapping paper`` (line: string, expected: int) =
+    [<Theory>]
+    [<InlineData("2x3x4", 58)>]
+    [<InlineData("1x1x10", 43)>]
+    let ``Test calculating the surface area of the wrapping paper`` (line: string, expected: int) =
         let boxDimensions = parseLine line
         calculateWrappingPaper boxDimensions |> should equal expected
 
-    [<TestMethod>]
-    [<DataRow("2x3x4", 34)>]
-    [<DataRow("1x1x10", 14)>]
-    member this.``Test calculating the of ribbon needed`` (line: string, expected: int) =
+    [<Theory>]
+    [<InlineData("2x3x4", 34)>]
+    [<InlineData("1x1x10", 14)>]
+    let ``Test calculating the of ribbon needed`` (line: string, expected: int) =
         let boxDimensions = parseLine line
         calculateRibbonLength boxDimensions |> should equal expected
 
-[<TestClass>]
-type Day03Tests () =
+module Day03Tests =
 
-    [<TestMethod>]
-    [<DataRow(">", 2)>]
-    [<DataRow("^>v<", 4)>]
-    [<DataRow("^v^v^v^v^v", 2)>]
-    member this.``Test delivering presents path`` (line: string, expected: int) =
+    [<Theory>]
+    [<InlineData(">", 2)>]
+    [<InlineData("^>v<", 4)>]
+    [<InlineData("^v^v^v^v^v", 2)>]
+    let ``Test delivering presents path`` (line: string, expected: int) =
         housesVisitedCount line |> should equal expected
 
     // [<TestMethod>]
--- AdventOfCode.Tests/CommonFunctionsTests.fs
+++ AdventOfCode.Tests/CommonFunctionsTests.fs
@@ -2,36 +2,36 @@
 
 open CommonFunctions
 open EverybodyCodes.Common
-open FsUnit.MsTest
-open Microsoft.VisualStudio.TestTools.UnitTesting
+open Xunit
+open FsUnit.Xunit
 
-[<TestMethod>]
-[<DataRow("abcdef", "609043", "000001DBBFA3A5C83A2D506429C7B00E")>]
-[<DataRow("pqrstuv", "1048970", "000006136EF2FF3B291C85725F17325C")>]
+[<Theory>]
+[<InlineData("abcdef", "609043", "000001DBBFA3A5C83A2D506429C7B00E")>]
+[<InlineData("pqrstuv", "1048970", "000006136EF2FF3B291C85725F17325C")>]
 let ``Test calculating an MD5 hash from a seed and suffix`` (seed: string, suffix: string, expected: string) =
     let md5Hash = calcMd5HashWithSeed seed suffix
     md5Hash |> should equal expected
 
-[<TestMethod>]
+[<Fact>]
 let ``Test grid from a 1D array and row size chunks correcctly`` () =
     let arrayData = [|1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12|]
     let grid = Grid(arrayData, 4)
     grid.Raw.Count |> should equal 3
     grid.Raw.[0].Count |> should equal 4
 
-[<TestMethod>]
+[<Fact>]
 let ``Test Points with the same values should be equal`` () =
     Point(3, 8) |> should equal (Point(3, 8))
 
-[<TestMethod>]
+[<Fact>]
 let ``Test Points with different values should not be equal`` () =
     Point(2, 8) = Point(3, 8) |> should be False
 
-[<TestMethod>]
-[<DataRow(0, 0, '.')>]
-[<DataRow(1, 1, 'F')>]
-[<DataRow(1, 2, '|')>]
-[<DataRow(3, 1, '7')>]
+[<Theory>]
+[<InlineData(0, 0, '.')>]
+[<InlineData(1, 1, 'F')>]
+[<InlineData(1, 2, '|')>]
+[<InlineData(3, 1, '7')>]
 let ``Test Grid get point`` (x: int) (y: int) (expected: char) =
     let sampleGridData = """.....
 .F-7.
@@ -41,12 +41,12 @@ let ``Test Grid get point`` (x: int) (y: int) (expected: char) =
     let grid = sampleGridData.Split() |> Grid<char>.FromStringLists
     grid.Get(x, y) |> should equal expected
 
-[<TestMethod>]
-[<DataRow(0, 0, true)>]
-[<DataRow(1, 1, true)>]
-[<DataRow(1, 2, true)>]
-[<DataRow(-1, 2, false)>]
-[<DataRow(5, 1, false)>]
+[<Theory>]
+[<InlineData(0, 0, true)>]
+[<InlineData(1, 1, true)>]
+[<InlineData(1, 2, true)>]
+[<InlineData(-1, 2, false)>]
+[<InlineData(5, 1, false)>]
 let ``Test Grid in bounds`` (x: int) (y: int) (expected: bool) =
     let sampleGridData = """.....
 .F-7.
@@ -56,7 +56,7 @@ let ``Test Grid in bounds`` (x: int) (y: int) (expected: bool) =
     let grid = sampleGridData.Split() |> Grid<char>.FromStringLists
     grid.InBounds(x, y) |> should equal expected
 
-[<TestMethod>]
+[<Fact>]
 let ``Test Grid adjacent points`` () =
     let sampleGridData = """.....
 .F-7.
