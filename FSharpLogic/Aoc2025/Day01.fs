d9ca85232217d7555ce6734a0cf9e9fce8db9e30
Joshua Rakita <rakitaj@users.noreply.github.com>
2025-12-02 00:10:10 -0800

    F# first problem, idiomatic. Kinda.

--- FSharpLogic/Aoc2025/Day01.fs
+++ FSharpLogic/Aoc2025/Day01.fs
@@ -1,1 +1,43 @@
 module Aoc2025.Day01
+
+let data = DataLoaders.readFileLinesStr 2025 01
+
+let parse (line: string) : Result<int, string> =
+    let amount = int line.[1..]
+    match line.[0] with
+    | 'L' -> Ok (amount * -1)
+    | 'R' -> Ok amount
+    | c -> Error $"Unknown direction '{c}'"
+
+
+// let calculatePart1 (lines: string seq) : int = 
+//     let parsedAmounts = lines |> Seq.choose parse
+//     let _, zeroCount = parsedAmounts |> Seq.fold ()
+    // let pos = 50
+    // let zeroCount = 0
+    // for amount in amounts {
+    //     pos += amount
+    //     pos = pos % 100
+    //     if pos == 0 {
+    //         zeroCount += 1
+    //     }
+    // }
+    // zeroCount
+let rec calc (pos: int) (zeroCount: int) (parsedAmounts: int list)  : int =
+    match parsedAmounts with
+    | [] -> zeroCount
+    | head :: tail -> 
+        let pos = (pos + head) % 100
+        let zeroCount = zeroCount + if pos = 0 then 1 else 0
+        calc pos zeroCount tail
+
+let part1Solution : int =
+    data
+    |> Seq.map parse
+    |> Seq.choose (function
+        | Ok v -> Some v
+        | Error e -> 
+            printfn "Failed: %s" e
+            None)
+    |> Seq.toList
+    |> calc 50 0
--- FSharpLogic/Program.fs
+++ FSharpLogic/Program.fs
@@ -1,6 +1,10 @@
-﻿printfn $"Day 01-1 {Aoc2015.Day01.part1Solution}"
+﻿printfn $"Advent Of Code 2015"
+printfn $"Day 01-1 {Aoc2015.Day01.part1Solution}"
 printfn $"Day 01-2 {Aoc2015.Day01.part2Solution}"
 printfn $"Day 02-1 {Aoc2015.Day02.part1Solution}"
 printfn $"Day 02-2 {Aoc2015.Day02.part2Solution}"
 printfn $"Day 03-1 {Aoc2015.Day03.part1Solution}"
 printfn $"Day 03-2 {Aoc2015.Day03.part2Solution}"
+
+printfn $"Advent Of Code 2025"
+printfn $"Day 01-1 {Aoc2025.Day01.part1Solution}"
